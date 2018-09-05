ce579b3152a87a47717903640d4cbce3eb39295d
Israel Junior <israeljrs@gmail.com>
2018-09-05 20:09:26 -0300

    make little ajustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
+// Declara a lib do videojs como externa ao angular
 declare let videojs: any;
 
 @Component({
@@ -9,18 +10,23 @@ declare let videojs: any;
 })
 export class AppComponent implements AfterViewInit {
 
-  title = 'sample-player';
+  // Titulo do component
+  title = 'Player com Video.JS';
+  // Instancia do video.js.
   vidObj: any;
+  // Poster para ser usado no video.js
   poster = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/poster.png';
+  // URL do video a ser reproduzido.
   video = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4';
+  // Acessa o elemento video do html5 via viewchild.
   @ViewChild('myvid') vid: ElementRef;
 
   ngAfterViewInit() {
     const options = {
       controls: true,
-      autoplay: true,
+      autoplay: false,
       preload: 'auto',
-      techOrder: ['html5', 'flash']
+      techOrder: ['html5']
     };
 
     this.vidObj = new videojs(this.vid.nativeElement, options, function onPlayerReady() {
